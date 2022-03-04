4e659fabe30fb0ecdefe2aa09f4b4617290dbe92
s.iormetti <s.iormetti@be-tse.it>
2022-03-04 12:34:57 +0100

    Ghttps://github.com/save1970save/firstapp-intellij.gitirst Commit

---
+++ src/it/swan/Main.java
@@ -0,0 +1,15 @@
+package it.swan;
+
+import it.swan.model.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World !!!");
+
+    Product product = new Product("Keybooard");
+    System.out.println(product.getName());
+
+    }
+
+}
---
+++ src/it/swan/model/Product.java
@@ -0,0 +1,13 @@
+package it.swan.model;
+
+public class Product {
+    private final String name;
+
+    public Product(String name ){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
